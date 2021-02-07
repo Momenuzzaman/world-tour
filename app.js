b5ea47178b4e35f90ce371617f54c72673c0bfc2
Md.Mommenuzzaman Emon <mdeamon4@gmail.com>
2021-02-07 16:58:00 +0600

    world tour

---
+++ app.js
@@ -0,0 +1,48 @@
+fetch("https://restcountries.eu/rest/v2/all")
+.then(res => res.json())
+.then(data => displayCountries(data));
+
+
+const displayCountries = countries =>{
+    const countriesDiv = document.getElementById("countries");
+
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        const countryDiv = document.createElement("div");
+        countryDiv.className = 'country'
+
+        const countryInfo = `
+            <h3 class = "country-name">${country.name}</h3>
+            <p>${country.capital}</p>
+            <button onclick ="displayCountryDetail('${country.name}')">Show detail</button>
+        `
+        countryDiv.innerHTML = countryInfo
+
+        countriesDiv.appendChild(countryDiv)
+
+    }
+}
+
+const displayCountryDetail = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderCountryInfo(data[0]));
+}
+
+const renderCountryInfo = country => {
+    
+    const countryDiv = document.getElementById("countryDetail");
+    countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population:${country.population}</p>
+        <P>Area:${country.area}</P>
+        <img src="${country.flag}">
+
+    `
+}
+
+
+
+
+
